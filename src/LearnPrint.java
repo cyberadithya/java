f078dfe8c6235aac304114f237badd7aefd6bcb8
cyberadithya <cyber.adithya@gmail.com>
2025-01-01 20:37:00 -0500

    Third Commit

---
+++ src/LearnPrint.java
@@ -0,0 +1,16 @@
+public class LearnPrint {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("This will print a string on the same line. ");
+        System.out.println("This will print an additional string on the same line.");
+        System.out.println(2*2);
+        System.out.println(96);
+
+        //This is a single line comment. It is written using 2 slashes.
+
+        /*This is a multi line comment
+        It lasts multiple lines and begins and ends with
+        a star and a slash.
+         */
+    }
+}
